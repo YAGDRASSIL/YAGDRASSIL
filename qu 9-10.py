ca45b4e4c07c4fd3b2ce1e253dd7c92e9ae7f277
Kirill Logvinov <95069717+YAGDRASSIL@users.noreply.github.com>
2021-12-03 10:13:30 +0300

    Add files via upload

---
+++ qu 9-10.py
@@ -0,0 +1,15 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=int(input())
+y=int(input())
+n=int(input())
+z=int(input())
+lengh=z*n
+#путь который был пройден
+lengh-=x
+#когда я буду у первого столба
+step=lengh//y
+#кол-во столбов которые я пройду
+if step<0
+step=0
+print(step)
